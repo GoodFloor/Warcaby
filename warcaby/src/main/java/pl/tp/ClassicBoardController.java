bb43d38b592e55d3990bb5b065cbf662c5ed930d
GoodFloor <lukasz_machnik@interia.eu>
2022-12-14 23:31:50 +0100

    More of game logic

--- warcaby/src/main/java/pl/tp/ClassicBoardController.java
+++ warcaby/src/main/java/pl/tp/ClassicBoardController.java
@@ -32,22 +32,40 @@ public class ClassicBoardController extends BoardController {
     private int getPositionY(String position) {
         return Integer.parseInt(position.substring(1, position.length())) - 1;
     }
+    private String codePosition(int posX, int posY) {
+        String result = "";
+        result += (char) (posX + 65);
+        result += posY + 1;
+        return result;
+    }
     public boolean isWhite(String position) {
         return board.getBoard()[this.getPositionX(position)][this.getPositionY(position)].type == "White";
     }
     public boolean isRed(String position) {
         return board.getBoard()[this.getPositionX(position)][this.getPositionY(position)].type == "Red";
     }
-    public boolean canKill(String position) {
+    private boolean canKill(int posX, int posY) {
         Piece[][] tempBoard = board.getBoard();
-        int posX = this.getPositionX(position);
-        int posY = this.getPositionY(position);
-        if(tempBoard[posX][posY].getClass() == Piece.class) {
+        if(posX > 1 && posY < 6 && tempBoard[posX - 1][posY - 1].getClass() == Piece.class && tempBoard[posX][posY].type != tempBoard[posX - 1][posY - 1].type && tempBoard[posX - 2][posY - 2] == null) {
             return true;
         }
-        return false;
+        else if(posX < 6 && posY < 6 && tempBoard[posX + 1][posY + 1].getClass() == Piece.class && tempBoard[posX][posY].type != tempBoard[posX + 1][posY + 1].type && tempBoard[posX + 2][posY + 2] == null) {
+            return true;
+        }
+        else return false;
     }
-
+    public String whoCanKill(String type) {
+        Piece[][] tempBoard = board.getBoard();
+        String result = "";
+        for (int i = 0; i < tempBoard.length; i++) {
+            for (int j = 0; j < tempBoard[i].length; j++) {
+                if(tempBoard[i][j].type == type && canKill(i, j)) {
+                    result += this.codePosition(i, j) + ";";
+                }
+            }
+        }
+        return result;
+    }
     public SquareStateEnum[][] translateBoard() {
         SquareStateEnum[][] result = new SquareStateEnum[board.getWidth()][board.getHeight()];
         Piece[][] boardContent = board.getBoard();
--- warcaby/src/test/java/pl/tp/ClassicBoardControllerTest.java
+++ warcaby/src/test/java/pl/tp/ClassicBoardControllerTest.java
@@ -1,17 +1,12 @@
 package pl.tp;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class ClassicBoardControllerTest {
 
-    @Test
-    public void canKill() {
-        ClassicBoardController testSubject = new ClassicBoardController();
-        testSubject.resetBoard();
-
-        assertTrue(testSubject.canKill("A1"));
-    }
+    
     
 }
