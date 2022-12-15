e2d085c7aba97f22d449b11a462cd41cf777cb6d
= <=>
2022-12-15 16:44:05 +0100

    printing error better

--- warcaby/src/main/java/pl/tp/GameController.java
+++ warcaby/src/main/java/pl/tp/GameController.java
@@ -35,7 +35,7 @@ public abstract class GameController {
             try {
                 boardController.movePiece(move[0], move[1]);
             } catch (IncorrectPositionException e) {
-                gameView.printException(e);
+                gameView.printMessage("Błędny ruch - spróbuj ponownie");
                 continue;
             }
             isWhiteTurn = !isWhiteTurn;
