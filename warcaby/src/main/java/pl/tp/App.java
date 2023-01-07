7030bfb042f0665109dd9dcf5dcc4ef0ab285511
= <=>
2023-01-07 18:38:56 +0100

    add basic javadoc

--- warcaby/src/main/java/pl/tp/App.java
+++ warcaby/src/main/java/pl/tp/App.java
@@ -1,7 +1,15 @@
 package pl.tp;
 
+/**
+ * Klasa uruchamiająca serwer
+ */
 public class App {
 
+    /**
+     * Funkcja main
+     * 
+     * @param args tablica argumentów przekazywanych do aplikacji
+     */
     public static void main(String[] args) {
         VersionPicker factory = new VersionPicker();
         GameController game = factory.getGame(1);
