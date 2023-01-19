34eb176e25205ffa4b313ced84df1bbcefdc28d5
GoodFloor <lukaszmachnik2@gmail.com>
2023-01-19 02:04:19 +0100

    diagrams

---
+++ warcaby/src/test/java/pl/tp/client/FieldTest.java
@@ -0,0 +1,26 @@
+package pl.tp.client;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+
+import org.junit.Test;
+
+
+public class FieldTest {
+
+    @Test
+    public void fieldSetActionTest1() {
+        BoardLayer boardLayer = new BoardLayer(null);
+        boardLayer.drawNew(8);
+        Field testField = new Field(5, 5, boardLayer);
+        assertEquals("F3", testField.getActionCommand());
+    }
+    @Test
+    public void fieldSetActionTest2() {
+        BoardLayer boardLayer = new BoardLayer(null);
+        boardLayer.drawNew(8);
+        Field testField = new Field(1, 0, boardLayer);
+        assertEquals("B8", testField.getActionCommand());
+    }
+}
