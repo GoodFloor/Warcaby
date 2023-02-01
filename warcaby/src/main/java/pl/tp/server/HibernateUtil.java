2dee1803783ccfac76077972557183f704b9a4bd
GoodFloor <lukaszmachnik2@gmail.com>
2023-02-01 23:41:57 +0100

    hibernate but still doesnt work

--- warcaby/src/main/java/pl/tp/server/HibernateUtil.java
+++ warcaby/src/main/java/pl/tp/server/HibernateUtil.java
@@ -5,23 +5,27 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.service.spi.ServiceException;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+        // Configuration con = new Configuration().configure();
+        // sessionFactory = con.buildSessionFactory();
+        // return sessionFactory;
         try {
             if (sessionFactory == null) {
+        
                 StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure().build();
                 Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
             }
             return sessionFactory;
-        } catch (ServiceException e) {
-            return null;
         } catch (Exception e) {
-            throw e;
+            e.printStackTrace();
+            return null;
         }
     }
     public static SessionFactory getSessionFactory() {
